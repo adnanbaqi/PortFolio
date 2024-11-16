229fba1230bda2ebcfaaa101afbe213cf05c2a8e
Md Adnan Baqi <adnan.baki786@gmail.com>
2024-11-17 03:09:11 +0530

    trial

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { TypeAnimation } from "react-type-animation";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import heroImage from "public/images/hero-image.png";
 
 
 const HeroSection = () => {
@@ -71,9 +72,8 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="./images/hero-image.png"
-              alt=""
-              title="hero-image"
+              src="heroImage"
+              alt="Hero"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={400}
               height={400}
