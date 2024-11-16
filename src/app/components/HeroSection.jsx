da88bd30c3231653c05c65838fa3257256f974ba
Md Adnan Baqi <adnan.baki786@gmail.com>
2024-11-17 03:19:22 +0530

    fix

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -71,7 +71,7 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/hero.png"
+              src="/images/hero-image.png"
               alt="Hero"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={400}
